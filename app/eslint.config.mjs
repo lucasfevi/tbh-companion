a1dbccbc5184e9b998c38158e87b745874c56bbc
Lucas Vieira <newlucasfevi@gmail.com>
2026-06-21 19:09:57 -0300

    chore(qa): ignore storybook-static/ in ESLint and Prettier
    build-storybook output isn't excluded from either tool's ignore list,
    so running it locally before pnpm run qa floods lint/format:check
    with thousands of false positives from linting minified bundles.

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -7,7 +7,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ["out/**", "release/**", "dist/**", "node_modules/**"],
+    ignores: ["out/**", "release/**", "dist/**", "node_modules/**", "storybook-static/**"],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
